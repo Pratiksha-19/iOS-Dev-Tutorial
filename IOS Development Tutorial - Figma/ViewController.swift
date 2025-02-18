13386cfbdeaf848a35b74535761b86d1ec73a626
Pratiksha Katyayni <pratikshapandey323@gmail.com>
2025-02-19 02:48:45 +0530

    iOSDev/Figma  Initial commit

---
+++ IOS Development Tutorial - Figma/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IOS Development Tutorial - Figma
+//
+//  Created by Pratiksha Katyayni on 18/02/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        //
+    }
+
+
+}
+
